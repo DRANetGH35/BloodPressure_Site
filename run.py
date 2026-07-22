e62f193cef638c4d85b8ddaf5c81d17b62bdb298
Grok <devanaptaker@gmail.com>
2026-07-22 09:42:15 -0700

    fix: change host and port to servers configuration to prevent 'unstaged changes' error with git

--- run.py
+++ run.py
@@ -20,5 +20,5 @@ def get_ip():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002, host=get_ip())
+    app.run(debug=True, port=8002, host='127.0.0.1')
     #                              host='192.168.86.53'
