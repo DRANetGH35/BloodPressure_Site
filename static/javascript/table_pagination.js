2ed5a9f9ac78c9c2a222172c0ab2dad5b823003f
Grok <devanaptaker@gmail.com>
2026-07-24 12:50:39 -0700

    add: page up and page down events

--- static/javascript/table_pagination.js
+++ static/javascript/table_pagination.js
@@ -1,18 +1,29 @@
 const bloodPressureTable = document.getElementById('blood-pressure-table')
+const pageUpBtn = document.getElementById('page-up')
+const pageDownBtn = document.getElementById('page-down')
+const pageNumInput = document.getElementById('page-num-input')
 
-page_num = 1
-const USFormatter = new Intl.DateTimeFormat('en-US', {
-  dateStyle: 'short',
-  timeStyle: 'medium'
-});
+pageNumInput.addEventListener('keyup', changePage)
+pageUpBtn.addEventListener('click', pageUp)
+pageDownBtn.addEventListener('click', pageDown)
 
+function pageUp() {
+    pageNumInput.value = parseInt(pageNumInput.value) + 1
+    changePage()
+}
+function pageDown(){
+    if (pageNumInput.value > 1){
+        pageNumInput.value = parseInt(pageNumInput.value) - 1
+    }
+    changePage()
+}
 
-
-
-url = `fetch_table_data/${page_num}`
-fetch(url).then(response => response.json()).then(data => {
+function changePage(){
+    bloodPressureTable.innerHTML = ""
+    page_num = pageNumInput.value;
+    url = `fetch_table_data/${page_num}`
+    fetch(url).then(response => response.json()).then(data => {
         response = data['data']
-        console.log(response)
         for (i=0; i<response.length; i++){
             entry = response[i]
             let newRow = bloodPressureTable.insertRow();
@@ -32,3 +43,6 @@ fetch(url).then(response => response.json()).then(data => {
             noteCell.textContent = entry.notes;
         }
     })
+}
+changePage()
+
