1dbc45c47a7ed24733381c2ac9c4c1032366476a
Devan <devanaptaker@gmail.com>
2026-08-04 21:52:23 -0700

    remove unnecessary print statements

--- routes.py
+++ routes.py
@@ -51,8 +51,6 @@ def table():
     page = 1
     per_page = 25
     table_data = BloodPressure.query.order_by(BloodPressure.created.desc()).paginate(page=page, per_page=per_page, error_out=False)
-    for entry in table_data:
-        print(entry.systolic, entry.diastolic)
     return render_template('table.html', table=table_data)
 
 @app.route('/graph_table')
