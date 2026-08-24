bbc1c4bb1f5cd51d279bcc6ef0293b9d82426440
Devan <devanaptaker@gmail.com>
2026-08-24 08:03:10 -0700

    fix: javascript fetch error

--- static/javascript/submitMedication.js
+++ static/javascript/submitMedication.js
@@ -8,7 +8,7 @@ medicationForm.addEventListener('formdata', (event) => {
     formData.append('time', formattedTime)
 })
 
-url = `${window.location.origion}/fetch_medication_entries`
+url = `${window.location.origin}/fetch_medication_entries`
 fetch(url).then(response => response.json()).then(data => {
     categories = []
     medications = data['medications']
